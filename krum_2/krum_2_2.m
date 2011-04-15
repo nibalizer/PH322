f0cb4fcdd179a6db29dbeda50cf03f8a7f266b21
Spencer Krum <krum.spencer@gmail.com>
2011-04-14 23:28:22 -0700

    oh fuk no time

--- krum_2/krum_2_2.m
+++ krum_2/krum_2_2.m
@@ -1,59 +1,113 @@
 %Spencer Krum
-%PH 322 problem 1_4
-
+%PH 322 problem 2_9
+%to solve a  cannon shot through air
 %general form of euler Val(t + dt) = val(T) + dval(t) * dt
 
-%solve numerkcally
-%dNa/dt = - Na / tau_a
-%dNb/dt = Na / tau_a - Nb / tau_b
+%solve numerically
+%and reproduce fig 2.5
 
-% urankum 238 has a halflkfe of 4.5 bkllkon years and decays to thorkum 234
-% thorkum 234 has a halflkfe of 24 days
-
-% halflkfe = ln(2) * tau
-% tau = halflkfe / ln(2)
-% ln(2) ~ 0.69315
 clear
 close all
 
-halflife_uranium_238 = 4.5 * 1000000000 * 365 * 24 * 60 * 60;
-halflife_thorium_234 =  24 * 24 * 60 * 60;
+fprintf('%s', 'Launching a Cannon ball with air resistance') 
 
-tau_u238 = halflife_uranium_238 / log(2);
-tau_t234 = halflife_thorium_234 / log(2);
+%setup consts
+a = 6.5e-3
+To = 25
+alpha = 2.5
+
+
+%b2 = b2/m
+b2 = 4e-5
 
 
 
-
-dt = input('Please enter your delta-t value: ')
-t2 = input('Please enter your t-final value: ')
-tau_a = input('Please enter your tau_a value: ')
-tau_b = input('Please enter your tau_b value: ')
-
-%setup tkme domakn
-%dt = 1e+6;
+%setup time domain
+dt = .01;
 t1 = 0;
-%t2 = 1e+18;
+t2 = 12;
 t = t1:dt:t2;
 
-%asskgn tau_a and tau_b
-%tau_a = tau_u238;
-%tau_b = tau_t234;
+%initial speed is composed of vx and vy initial
+x(1) = 0;
+vx(1) = 30;
+vy(1) = 30;
+y(1) = 0;
+v(1) = sqrt((vx(1)).^2 + (vy(1)).^2);
+g = 9.8
 
-disp(tau_a)
-disp(tau_b)
-
-% lets start wkth a mole of u238
-% lets start wkth zero moles of t234
-Na(1) = 6.022e+23;
-Nb(1) = 0;
-
-
+%for loop three times
 for k=1:length(t)-1,
-    Na(k+1) = Na(k) + -Na(k) / tau_a *dt;
-    Nb(k+1) = Nb(k) + ((Na(k) / tau_a) + (-Nb(k) / tau_b)) *dt;
-    fprintf('T:\t%f\tNa:\t%f\t\tNb:\t%f\n', t(k), Na(k), Nb(k))
+    b2 = 4e-5;
+    po = 1;
+    p = po *(1 - a * y(k) / To).^alpha;
+    b2 = b2 * p/po;
+    v(k) = sqrt((vx(k)).^2 + (vy(k)).^2);
+
+    x(k + 1) = x(k) + vx(k) * dt;
+    vx(k +1) = vx(k) + - b2 * v(k) * vx(k) *dt;
+    y(k + 1) = y(k) + vy(k) * dt;
+    vy(k +1) = vy(k) + - b2 * v(k) * vy(k) *dt - g*dt;
+    fprintf('T:\t%f\tV:\t%f\n', t(k),v(k))
+    if length(y) > 100
+        if y(k) < 0
+            break
+        end
+    end
+
 end
 
-plot(t,Na, 'r', t, Nb, 'b')
+x1(1) = 0;
+vx1(1) = 20;
+vy1(1) = 40;
+y1(1) = 0;
+v(1) = sqrt((vx1(1)).^2 + (vy1(1)).^2);
+g = 9.8
+
+for k=1:length(t)-1,
+    b2 = 4e-5;
+    po = 1;
+    p = po *(1 - a * y1(k) / To).^alpha;
+    b2 = b2 * p/po;
+    v(k) = sqrt((vx1(k)).^2 + (vy1(k)).^2);
+
+    x1(k + 1) = x1(k) + vx1(k) * dt;
+    vx1(k +1) = vx1(k) + - b2 * v(k) * vx1(k) *dt;
+    y1(k + 1) = y1(k) + vy1(k) * dt;
+    vy1(k +1) = vy1(k) + - b2 * v(k) * vy1(k) *dt - g*dt;
+    fprintf('T:\t%f\tV:\t%f\n', t(k),v(k))
+    if length(y1) > 100 && y1(k) < 0
+        break
+    end
+
+end
+
+x2(1) = 0;
+vx2(1) = 40;
+vy2(1) = 20;
+y2(1) = 0;
+v(1) = sqrt((vx2(1)).^2 + (vy2(1)).^2);
+g = 9.8
+
+for k=1:length(t)-1,
+    b2 = 4e-5;
+    po = 1;
+    p = po *(1 - a * y2(k) / To).^alpha;
+    b2 = b2 * p/po;
+    v(k) = sqrt((vx2(k)).^2 + (vy2(k)).^2);
+
+    x2(k + 1) = x2(k) + vx2(k) * dt;
+    vx2(k +1) = vx2(k) + - b2 * v(k) * vx2(k) *dt;
+    y2(k + 1) = y2(k) + vy2(k) * dt;
+    vy2(k +1) = vy2(k) + - b2 * v(k) * vy2(k) *dt - g*dt;
+    fprintf('T:\t%f\tV:\t%f\n', t(k),v(k))
+    if length(y2) > 100 && y2(k) < 0
+        break
+    end
+
+end
+
+plot(x,y, 'r', x1,y1,'b', x2,y2,'g')
+%plot(x,y, 'r')
+legend('The x,y coordinate of a cannonball in air init cond vx0=30, vy0=30','The x,y coordinate of a cannonball in air init cond vx0=40, vy0=20','The x,y coordinate of a cannonball in air init cond vx0=20, vy0=40')
 pause()
